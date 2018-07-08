6d1d7beedc2bd44ab9dc2b47888c1075158bc59b
Luay Adam <luay985@gmail.com>
2018-07-08 13:35:28 +0200

    addind New & Update functions

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,27 @@
 // Your code goes here!
+let TODOS = [];
+
+function update() {
+    const $todoList = document.querySelector('.todo-list');
+    $todoList.innerHTML = '';
+    for (const item of TODOS) {
+        console.log(item);
+        const $li = document.createElement('li');
+        $li.innerHTML = item;
+        $todoList.appendChild($li);
+    }
+    document.querySelector('.main').style.display = 'block';
+}
+
+function onNewTodo(e) {
+    const title = e.target.value;
+    console.log(title);
+    // same as this line
+    // document.querySelector('.new-todo').value;
+    TODOS.push(title);
+    update();
+}
+
+// Select the new todo input field
+const $newTodo = document.querySelector('.new-todo');
+$newTodo.addEventListener('change', onNewTodo);
